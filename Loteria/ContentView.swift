f4bff2289c47399405068b7ce2d20608776cbdfd
Roberto Filho <roberto.siilva@hotmail.com>
2023-02-24 09:56:38 -0300

    Initial Commit

---
+++ Loteria/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Loteria
+//
+//  Created by Roberto Filho on 24/02/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Loteria/LoteriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoteriaApp.swift
+//  Loteria
+//
+//  Created by Roberto Filho on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LoteriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
